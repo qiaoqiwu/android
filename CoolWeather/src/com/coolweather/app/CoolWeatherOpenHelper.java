0daf345aaad0be8eb263f60b39c2b7d7371cfcfc
qiaoqiwu <wqqtest001@163.com>
2016-05-17 20:30:53 +0800

    coolweather

---
+++ CoolWeather/src/com/coolweather/app/CoolWeatherOpenHelper.java
@@ -0,0 +1,44 @@
+package com.coolweather.app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+	
+	//创建Province表
+	public static final String CREATE_PROVINCE = "create table Province ("
+			+ "id integer primary key AUTOINCREMENT, "
+			+ "province_name text, "
+			+ "province_code text)";
+	//创建City表
+	public static final String CREATE_CITY = "create table City ("
+			+ "id integer primary key AUTOINCREMENT, "
+			+ "city_name text, "
+			+ "city_code text, "
+			+ "province_id integer)";
+	//创建County表
+	public static final String CREATE_COUNTY = "create table County ("
+			+ "id integer primary key AUTOINCREMENT, "
+			+ "county_name text, "
+			+ "county_code text, "
+			+ "city_id integer)";
+	
+	public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+	}
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("drop table if exists Province");
+		db.execSQL("drop table if exists City");
+		db.execSQL("drop table if exists County");
+		onCreate(db);
+	}
+}
---
+++ CoolWeather/src/com/coolweather/app/HttpCallbackListener.java
@@ -0,0 +1,6 @@
+package com.coolweather.app;
+
+public interface HttpCallbackListener {
+	void onFinish(String response);
+	void onError(Exception e);
+}
